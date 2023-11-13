791080b77b4601fd50ebc291f2d8c6ba8a8465fd
CCXander <103416424+CCXander@users.noreply.github.com>
2023-11-13 19:02:51 +0200

    Inital map menu implementation

--- GOD/addons/playerprefsutility/Demo/Scripts/PF_Demo.cs
+++ GOD/addons/playerprefsutility/Demo/Scripts/PF_Demo.cs
@@ -5,130 +5,130 @@ using System.Linq;
 
 public partial class PF_Demo : Node
 {
-    [Export] LineEdit keyField;
-    [Export] LineEdit valueField;
+	[Export] LineEdit keyField;
+	[Export] LineEdit valueField;
 
-    [Export] Button storeButton;
-    [Export] Button retrieveButton;
+	[Export] Button storeButton;
+	[Export] Button retrieveButton;
 
-    [Export] Label warningText;
-    [Export] Label emptyListLabel;
+	[Export] Label warningText;
+	[Export] Label emptyListLabel;
 
-    [Export] string defaultKey;
-    [Export] bool retrieveOnStart;
+	[Export] string defaultKey;
+	[Export] bool retrieveOnStart;
 
-    [Export] Control listParent;
-    //[Export] PF_Element elementTemplate;
-    [Export] Button deleteAllButton;
+	[Export] Control listParent;
+	//[Export] PF_Element elementTemplate;
+	[Export] Button deleteAllButton;
 
-    //Control listParent => elementTemplate.GetParent() as Control;
+	//Control listParent => elementTemplate.GetParent() as Control;
 
-    [Export] PackedScene elementPrefab;
+	[Export] PackedScene elementPrefab;
 
-    PF_Element prefab => elementPrefab.Instantiate() as PF_Element;
+	PF_Element prefab => elementPrefab.Instantiate() as PF_Element;
 
-    public override void _Ready()
-    {
-        warningText.Visible = false;
-        storeButton.Pressed += Store;
-        retrieveButton.Pressed += Retrieve;
+	public override void _Ready()
+	{
+		warningText.Visible = false;
+		storeButton.Pressed += Store;
+		retrieveButton.Pressed += Retrieve;
 
-        deleteAllButton.Pressed += () =>
-        {
-            PlayerPrefs.DeleteAll();
-            BuildList();
-        };
+		deleteAllButton.Pressed += () =>
+		{
+			PlayerPrefs.DeleteAll();
+			BuildList();
+		};
 
-        keyField.Text = defaultKey;
-        if (retrieveOnStart)
-            Retrieve();
+		keyField.Text = defaultKey;
+		if (retrieveOnStart)
+			Retrieve();
 
-        BuildList();
-    }
+		BuildList();
+	}
 
-    void Store ()
-    {
-        warningText.Visible = false;
+	void Store ()
+	{
+		warningText.Visible = false;
 
-        if (string.IsNullOrEmpty(keyField.Text.Trim()))
-            Warn("Key cannot be empty!");
-        else
-        {
-            PlayerPrefs.SetString(keyField.Text, valueField.Text);
-        }
+		if (string.IsNullOrEmpty(keyField.Text.Trim()))
+			Warn("Key cannot be empty!");
+		else
+		{
+			PlayerPrefs.SetString(keyField.Text, valueField.Text);
+		}
 
-        BuildList();
-    }
+		BuildList();
+	}
 
-    void Retrieve ()
-    {
-        warningText.Visible = false;
+	void Retrieve ()
+	{
+		warningText.Visible = false;
 
-        if (string.IsNullOrEmpty(keyField.Text.Trim()))
-            Warn("Key cannot be empty!");
-        else
-        {
-            valueField.Text = PlayerPrefs.GetString(keyField.Text.Trim());
-        }
-    }
+		if (string.IsNullOrEmpty(keyField.Text.Trim()))
+			Warn("Key cannot be empty!");
+		else
+		{
+			valueField.Text = PlayerPrefs.GetString(keyField.Text.Trim());
+		}
+	}
 
-    void Warn (string msg)
-    {
-        warningText.Text = msg;
-        warningText.Visible = true;
-    }
+	void Warn (string msg)
+	{
+		warningText.Text = msg;
+		warningText.Visible = true;
+	}
 
-    void BuildList ()
-    {
-        var pfs = PlayerPrefs.ListKeys.Clone() as string[];
-        IEnumerable<PF_Element> elements = listParent.GetChildren().Where(c => ((Control)c) is PF_Element).Select(c => ((Control)c as PF_Element)).Where(c => c.Visible);
+	void BuildList ()
+	{
+		var pfs = PlayerPrefs.ListKeys.Clone() as string[];
+		IEnumerable<PF_Element> elements = listParent.GetChildren().Where(c => ((Control)c) is PF_Element).Select(c => ((Control)c as PF_Element)).Where(c => c.Visible);
 
-        emptyListLabel.Visible = pfs.Length == 0;
+		emptyListLabel.Visible = pfs.Length == 0;
 
-        for(int i = 0; i < Mathf.Max(pfs.Length, elements.Count()); i++)
-        {
-            if (i < elements.Count() && i < pfs.Length)
-            {
-                string val = PlayerPrefs.GetString(pfs[i]);
+		for(int i = 0; i < Mathf.Max(pfs.Length, elements.Count()); i++)
+		{
+			if (i < elements.Count() && i < pfs.Length)
+			{
+				string val = PlayerPrefs.GetString(pfs[i]);
 
-                elements.ElementAt(i).SetElement(pfs[i], val);
-            }
-            else if (i < elements.Count())
-                elements.ElementAt(i).QueueFree();
-            else if (i < pfs.Length)
-            {
-                var element = prefab;
-                listParent.AddChild(element);
-                element.Visible = true;
-                string val = PlayerPrefs.GetString(pfs[i]);
-                element.SetElement(pfs[i], val);
-                int index = i;
-                element.AssignDeleteAction(() =>
-                {
-                    PlayerPrefs.DeleteValue(PlayerPrefs.ListKeys[index]);
-                    BuildList();
-                });
-                element.AssignMainButton((key) =>
-                {
-                    keyField.Text = key;
-                });
-            }
-        }
-    }
+				elements.ElementAt(i).SetElement(pfs[i], val);
+			}
+			else if (i < elements.Count())
+				elements.ElementAt(i).QueueFree();
+			else if (i < pfs.Length)
+			{
+				var element = prefab;
+				listParent.AddChild(element);
+				element.Visible = true;
+				string val = PlayerPrefs.GetString(pfs[i]);
+				element.SetElement(pfs[i], val);
+				int index = i;
+				element.AssignDeleteAction(() =>
+				{
+					PlayerPrefs.DeleteValue(PlayerPrefs.ListKeys[index]);
+					BuildList();
+				});
+				element.AssignMainButton((key) =>
+				{
+					keyField.Text = key;
+				});
+			}
+		}
+	}
 
-    void ClearList ()
-    {
-        var children = listParent.GetChildren();
-        if (children?.Count > 0)
-        {
-            foreach (var child in children)
-            {
-                Control ctrl = (Control)child;
-                if (ctrl.Visible)
-                {
-                    ctrl.QueueFree();
-                }
-            }
-        }
-    }
+	void ClearList ()
+	{
+		var children = listParent.GetChildren();
+		if (children?.Count > 0)
+		{
+			foreach (var child in children)
+			{
+				Control ctrl = (Control)child;
+				if (ctrl.Visible)
+				{
+					ctrl.QueueFree();
+				}
+			}
+		}
+	}
 }
