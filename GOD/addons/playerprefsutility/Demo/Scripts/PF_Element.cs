fed63548691d2c4643f9a2323948e7a30475a9e5
CCXander <103416424+CCXander@users.noreply.github.com>
2023-11-13 19:07:44 +0200

    Inital map menu implementation

--- GOD/addons/playerprefsutility/Demo/Scripts/PF_Element.cs
+++ GOD/addons/playerprefsutility/Demo/Scripts/PF_Element.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class PF_Element : Control
 {
-    [Export] Label keyLabel;
-    [Export] Label valueLabel;
-    [Export] Button mainButton;
-    [Export] Button deleteButton;
+	[Export] Label keyLabel;
+	[Export] Label valueLabel;
+	[Export] Button mainButton;
+	[Export] Button deleteButton;
 
-    public void SetElement (string key, string value)
-    {
-        keyLabel.Text = key;
-        valueLabel.Text = value;
-    }
+	public void SetElement (string key, string value)
+	{
+		keyLabel.Text = key;
+		valueLabel.Text = value;
+	}
 
-    public void AssignMainButton (Action<string> action)
-    {
-        mainButton.Pressed += () => action?.Invoke(keyLabel.Text);
-    }
+	public void AssignMainButton (Action<string> action)
+	{
+		mainButton.Pressed += () => action?.Invoke(keyLabel.Text);
+	}
 
-    public void AssignDeleteAction (Action action)
-    {
-        deleteButton.Pressed += () => action?.Invoke();
-    }
+	public void AssignDeleteAction (Action action)
+	{
+		deleteButton.Pressed += () => action?.Invoke();
+	}
 }
--- GOD/addons/playerprefsutility/PlayerPrefs.cs
+++ GOD/addons/playerprefsutility/PlayerPrefs.cs
@@ -14,186 +14,186 @@ using Microsoft.Win32;
 
 public static class PlayerPrefs
 {
-    //static string registyPath => $@"HKEY_CURRENT_USER\Software\{AppName}";
-    static string registyPath => $@"Software\{AppName}";
-    static string AppName
-    {
-        get
-        {
-            return ProjectSettings.GetSetting("application/config/name", "Godot Project").ToString();
-        }
-    }
+	//static string registyPath => $@"HKEY_CURRENT_USER\Software\{AppName}";
+	static string registyPath => $@"Software\{AppName}";
+	static string AppName
+	{
+		get
+		{
+			return ProjectSettings.GetSetting("application/config/name", "Godot Project").ToString();
+		}
+	}
 
-    public static string[] ListKeys
-    {
-        get
-        {
+	public static string[] ListKeys
+	{
+		get
+		{
 #if GODOT_WINDOWS
-            try
-            {
+			try
+			{
 #pragma warning disable CA1416
-                string[] subKeys = Registry.CurrentUser.OpenSubKey(registyPath).GetValueNames();
+				string[] subKeys = Registry.CurrentUser.OpenSubKey(registyPath).GetValueNames();
 #pragma warning restore CA1416
-                return subKeys;
-            } catch (Exception e)
-            {
-                GD.PrintErr(e);
-                return new string[0];
-            }
+				return subKeys;
+			} catch (Exception e)
+			{
+				GD.PrintErr(e);
+				return new string[0];
+			}
 #else
-            CheckDir();
-            DirAccess dir = DirAccess.Open(filePath);
-            return dir.GetFiles() ?? new string[0];   
+			CheckDir();
+			DirAccess dir = DirAccess.Open(filePath);
+			return dir.GetFiles() ?? new string[0];   
 #endif
-        }
-    }
+		}
+	}
 
 
-    public static void SetValue<T> (string key, T value)
-    {
+	public static void SetValue<T> (string key, T value)
+	{
 #if GODOT_WINDOWS
-        try
-        {
+		try
+		{
 #pragma warning disable CA1416
-            Registry.CurrentUser.OpenSubKey(registyPath, true).SetValue(key, value);
+			Registry.CurrentUser.OpenSubKey(registyPath, true).SetValue(key, value);
 #pragma warning restore CA1416
-        }
-        catch (Exception e) 
-        {
-            GD.PrintErr(e);
-        }
+		}
+		catch (Exception e) 
+		{
+			GD.PrintErr(e);
+		}
 #else
-        SetLocal(key, value.ToString());
+		SetLocal(key, value.ToString());
 #endif
-    }
+	}
 
-    public static T GetValue<T> (string key, T defaultValue = default(T))
-    {
-        try
-        {
+	public static T GetValue<T> (string key, T defaultValue = default(T))
+	{
+		try
+		{
 #if GODOT_WINDOWS
 #pragma warning disable CA1416
-            object val = Registry.CurrentUser.OpenSubKey(registyPath).GetValue(key);
+			object val = Registry.CurrentUser.OpenSubKey(registyPath).GetValue(key);
 #pragma warning restore CA1416
 #else
-            object val = GetLocal(key);
+			object val = GetLocal(key);
 #endif
 
-            if (val == null)
-            {
-                GD.PrintErr($"Player Prefs Key {key} not found!");
-                return defaultValue;
-            } else
-            {
-                if (typeof(T).IsPrimitive || typeof(T) == typeof(string))
-                {
-                    return (T)Convert.ChangeType(val, typeof(T));
-                }
-                else
-                {
-                    GD.PrintErr($"Cannot store data of type {typeof(T).Name} in PlayerPrefs");
-                    return defaultValue;
-                }
-            }
+			if (val == null)
+			{
+				GD.PrintErr($"Player Prefs Key {key} not found!");
+				return defaultValue;
+			} else
+			{
+				if (typeof(T).IsPrimitive || typeof(T) == typeof(string))
+				{
+					return (T)Convert.ChangeType(val, typeof(T));
+				}
+				else
+				{
+					GD.PrintErr($"Cannot store data of type {typeof(T).Name} in PlayerPrefs");
+					return defaultValue;
+				}
+			}
 
 
-        } catch (Exception e)
-        {
-            GD.PrintErr(e);
-            return defaultValue;
-        }
-    }
+		} catch (Exception e)
+		{
+			GD.PrintErr(e);
+			return defaultValue;
+		}
+	}
 
-    public static void DeleteValue (string key)
-    {
+	public static void DeleteValue (string key)
+	{
 #if GODOT_WINDOWS
-        try
-        {
+		try
+		{
 #pragma warning disable CA1416
-            RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(registyPath, true);
+			RegistryKey registryKey = Registry.CurrentUser.OpenSubKey(registyPath, true);
 #pragma warning restore CA1416
 
-            if (registryKey != null)
+			if (registryKey != null)
 #pragma warning disable CA1416
-                registryKey.DeleteValue(key);
+				registryKey.DeleteValue(key);
 #pragma warning restore CA1416
-            else
-                GD.Print($"Registry key '{key}' not found.");
-        }
-        catch (Exception ex)
-        {
-            GD.PrintErr($"Error deleting key: {ex.Message}");
-        }
+			else
+				GD.Print($"Registry key '{key}' not found.");
+		}
+		catch (Exception ex)
+		{
+			GD.PrintErr($"Error deleting key: {ex.Message}");
+		}
 #else
-        DeleteLocal(key);
+		DeleteLocal(key);
 #endif
-    }
+	}
 
-    public static void DeleteAll ()
-    {
-        var listCopy = ListKeys.Clone() as string[];
-        foreach(var key in listCopy)
-            DeleteValue(key);
-    }
+	public static void DeleteAll ()
+	{
+		var listCopy = ListKeys.Clone() as string[];
+		foreach(var key in listCopy)
+			DeleteValue(key);
+	}
 
-    public static void SetBool (string key, bool value) => SetValue(key, value);
-    public static bool GetBool (string key, bool defaultValue = false) => GetValue(key, defaultValue);
+	public static void SetBool (string key, bool value) => SetValue(key, value);
+	public static bool GetBool (string key, bool defaultValue = false) => GetValue(key, defaultValue);
 
-    public static void SetInt (string key, int value) => SetValue(key, value);
-    public static int GetInt (string key, int defaultValue = 0) => GetValue(key, defaultValue);
+	public static void SetInt (string key, int value) => SetValue(key, value);
+	public static int GetInt (string key, int defaultValue = 0) => GetValue(key, defaultValue);
 
-    public static void SetString (string key, string value) => SetValue(key, value);
-    public static string GetString (string key, string defaultValue = "") => GetValue(key, defaultValue);
+	public static void SetString (string key, string value) => SetValue(key, value);
+	public static string GetString (string key, string defaultValue = "") => GetValue(key, defaultValue);
 
-    public static void SetFloat (string key, float value) => SetValue(key, value);
-    public static float GetFloat (string key, float defaultValue = 0f) => GetValue(key, defaultValue);
+	public static void SetFloat (string key, float value) => SetValue(key, value);
+	public static float GetFloat (string key, float defaultValue = 0f) => GetValue(key, defaultValue);
 
-    #region Local Storage
-    public static string filePath => $"user://playerprefs/";
-    static void CheckDir()
-    {
-        DirAccess root = DirAccess.Open("user://");
-        if (!root.DirExists("playerprefs"))
-            root.MakeDir("playerprefs");
-    }
+	#region Local Storage
+	public static string filePath => $"user://playerprefs/";
+	static void CheckDir()
+	{
+		DirAccess root = DirAccess.Open("user://");
+		if (!root.DirExists("playerprefs"))
+			root.MakeDir("playerprefs");
+	}
 
-    static void SetLocal(string key, string value)
-    {
-        CheckDir();
+	static void SetLocal(string key, string value)
+	{
+		CheckDir();
 
-        string path = filePath + "/" + key;
+		string path = filePath + "/" + key;
 
-        FileAccess file = FileAccess.Open(path, FileAccess.ModeFlags.Write);
-        file.StoreString(value.ToString());
-        file.Close();
-    }
+		FileAccess file = FileAccess.Open(path, FileAccess.ModeFlags.Write);
+		file.StoreString(value.ToString());
+		file.Close();
+	}
 
-    static string GetLocal(string key)
-    {
-        CheckDir();
+	static string GetLocal(string key)
+	{
+		CheckDir();
 
-        string path = filePath + "/" + key;
+		string path = filePath + "/" + key;
 
-        if (FileAccess.FileExists(path))
-        {
-            FileAccess file = FileAccess.Open(path, FileAccess.ModeFlags.Read);
-            string text = file.GetAsText();
-            file.Close();
-            return text;
-        }
-        return null;
-    }
+		if (FileAccess.FileExists(path))
+		{
+			FileAccess file = FileAccess.Open(path, FileAccess.ModeFlags.Read);
+			string text = file.GetAsText();
+			file.Close();
+			return text;
+		}
+		return null;
+	}
 
-    static void DeleteLocal(string key)
-    {
-        CheckDir();
+	static void DeleteLocal(string key)
+	{
+		CheckDir();
 
-        string path = filePath + "/" + key;
-        if (FileAccess.FileExists(path))
-        {
-            DirAccess dir = DirAccess.Open(filePath);
-            dir.Remove(key);
-        }
-    }
-    #endregion
+		string path = filePath + "/" + key;
+		if (FileAccess.FileExists(path))
+		{
+			DirAccess dir = DirAccess.Open(filePath);
+			dir.Remove(key);
+		}
+	}
+	#endregion
 }
